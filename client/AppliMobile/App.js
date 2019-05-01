43eba7f32d0556bbce73b804bff2a6808450ff00
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2019-05-01 17:58:15 +0200

    connection front back opérationnel (attention IP) + architecture front

--- client/AppliMobile/App.js
+++ client/AppliMobile/App.js
@@ -1,13 +1,13 @@
-
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, TextInput, Button  } from 'react-native';
 import { Constants, MapView, Location, Permissions } from 'expo';
+import routeService from './src/services/routeService';
 
 export default class App extends React.Component {
   state = {
     mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
     locationResult: null,
-    location: {coords: { latitude: 37.78825, longitude: -122.4324}},
+    location: {coords: { latitude: 37.78825, longitude: -122.4324}}
   };
 
   componentDidMount() {
@@ -19,17 +19,21 @@ export default class App extends React.Component {
   };
 
   _getLocationAsync = async () => {
-   let { status } = await Permissions.askAsync(Permissions.LOCATION);
-   if (status !== 'granted') {
-     this.setState({
-       locationResult: 'Permission to access location was denied',
-       location,
-     });
-   }
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        locationResult: 'Permission to access location was denied',
+        location,
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ locationResult: JSON.stringify(location), location, });
+  };
 
-   let location = await Location.getCurrentPositionAsync({});
-   this.setState({ locationResult: JSON.stringify(location), location, });
- };
+  _getRoute = async () => {
+    const res = await routeService.getRoute();
+    console.log(res);
+  }
 
   render() {
     return (
@@ -41,20 +45,17 @@ export default class App extends React.Component {
           ref={map => {this.map = map}}
           showsUserLocation = {true}
           userTrackingMode = {true}
-          onRegionChange={this._handleMapRegionChange}
-
-
-          
+          onRegionChange={this._handleMapRegionChange}      
         >
-    <MapView.Marker
-      coordinate={this.state.location.coords}
-      title="My Marker"
-      description="Some description"
-    />
+        <MapView.Marker
+          coordinate={this.state.location.coords}
+          title="My Marker"
+          description="Some description"
+        />
         </MapView>
-            <TextInput style={{ marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5 }} placeholder='Où va-t-on ?'/>
-            <Button title='Rechercher' onPress={() => {}}/>
-             
+        <TextInput style={{ marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5 }} placeholder='Où va-t-on ?'/>
+        <Button title='Rechercher' onPress={() => {}}/>
+        <Button title='Test Connection' onPress={() => this._getRoute()}/>   
         <Text>
           Location: {this.state.locationResult}
         </Text>
