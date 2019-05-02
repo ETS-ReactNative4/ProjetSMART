6ebcacfef0c15da43951a2b6624b6c538115c973
TiboREM <thibault.remy@insa-lyon.fr>
2019-05-02 11:07:55 +0200

    initialisation front itinéraire

--- client/AppliMobile/App.js
+++ client/AppliMobile/App.js
@@ -1,82 +1,78 @@
-import React from 'react';
-import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
-import { Constants, MapView, Location, Permissions } from 'expo';
-import routeService from './src/services/routeService';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions
+} from 'react-native';
 
-export default class App extends React.Component {
-  state = {
-    mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
-    locationResult: null,
-    location: { coords: { latitude: 37.78825, longitude: -122.4324 } }
-  };
+import MapView from 'expo';
+import Polyline from '@mapbox/polyline';
+
+export default class RnDirectionsApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      coords: []
+    }
+  }
 
   componentDidMount() {
-    this._getLocationAsync();
+    // find your origin and destination point coordinates and pass it to our method.
+     this.getDirections("45.780090, 4.890325", " 43.8879, 8.0316")
   }
 
-  _handleMapRegionChange = (mapRegion) => {
-    this.setState({ mapRegion });
-  };
-
-  _getLocationAsync = async () => {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        locationResult: 'Permission to access location was denied',
-        location,
-      });
+  async getDirections(startLoc, destinationLoc) {
+        try {
+            let resp = await fetch(`https:/maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }`)
+            let respJson = await resp.json();
+            let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+            let coords = points.map((point, index) => {
+                return  {
+                    latitude : point[0],
+                    longitude : point[1]
+                }
+            })
+            this.setState({coords: coords})
+            return coords
+        } catch(error) {
+            alert(error)
+            return error
+        }
     }
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ locationResult: JSON.stringify(location), location, });
-  };
-
-  _getRoute = async () => {
-    const res = await routeService.getRoute();
-    console.log(res);
-  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          style={{ marginTop: 30, alignSelf: 'stretch', height: '90%' }}
-          // initialRegion={{ latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
-          region={this.state.region}
-          ref={(map) => { this.map = map; }}
-          showsUserLocation
-          userTrackingMode
-          onRegionChange={this._handleMapRegionChange}
-        >
-          <MapView.Marker
-            coordinate={this.state.location.coords}
-            title="My Marker"
-            description="Some description"
-          />
+      <View>
+        <MapView style={styles.map} initialRegion={{
+          latitude:41.0082, 
+          longitude:28.9784, 
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}>
+
+        <MapView.Polyline 
+            coordinates={this.state.coords}
+            strokeWidth={2}
+            strokeColor="red"/>
+
         </MapView>
-        <TextInput style={{ marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5 }} placeholder="Où va-t-on ?" />
-        <Button title="Rechercher" onPress={() => {}} />
-        <Button title="Test Connection" onPress={() => this._getRoute()} />
-        <Text>
-          Location: {this.state.locationResult}
-        </Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
   },
 });
+
+AppRegistry.registerComponent('RnDirectionsApp', () => RnDirectionsApp);
