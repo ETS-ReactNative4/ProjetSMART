04d2ea18fc34e6439f275c2a8fc30364ccb3f3e3
FloMut <34032667+FloMut@users.noreply.github.com>
2019-05-04 20:36:24 +0200

    ajout d'un choix entre deux signalements

--- client/AppliMobile/src/components/BoutonSignalement/BoutonSignalement.js
+++ client/AppliMobile/src/components/BoutonSignalement/BoutonSignalement.js
@@ -1,18 +1,77 @@
 import React from 'react';
-import { Button, Alert } from 'react-native';
-// import styles from './stylesBoutonSignalement';
+import { View, Button, Alert } from 'react-native';
+import styles from './stylesBoutonSignalement';
 
 export default class BoutonSignalement extends React.Component {
-  _onPress = () => {
-    Alert.alert("Et non! Ca n'est pas encore codé :'(");
+
+  state = {
+    start: true
+  }
+
+  _envoyerSignalement = (signalement, latitude, longitude, error) => {
+    const message = {
+      signalement: signalement,
+      latitude: latitude,
+      longitude: longitude,
+      error: error
+    }
+    Alert.alert( "" , JSON.stringify(message) );
+  }
+
+  _signaler = (signalement) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this._envoyerSignalement(signalement, position.coords.latitude, position.coords.longitude, null);
+      },
+      (error) => {
+        this._envoyerSignalement(signalement, null, null, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
+  _onPressSignalement = () => {
+    this.setState({start: false});
+  }
+
+  _onPressRetour = () => {
+    this.setState({start: true});
   }
 
   render() {
-    return (
-      <Button
-        onPress={this._onPress}
-        title="Signalement"
-      />
-    );
+    if(this.state.start)
+    {
+      return (
+        <View style={styles.container} >
+          <Button
+            style={styles.bouton}
+            onPress={this._onPressSignalement}
+            title="Signalement"
+          />
+        </View>
+      );
+    }
+    else
+    {
+      return (
+        <View style={styles.container} >
+          <Button
+            style={styles.bouton}
+            onPress={() => {this._signaler("Eclairage")} }
+            title="Eclairage"
+          />
+          <Button
+            style={styles.bouton}
+            onPress={() => {this._signaler("Travaux")} }
+            title="Travaux"
+          />
+          <Button
+            style={styles.bouton}
+            onPress={this._onPressRetour}
+            title="Retour"
+          />
+        </View>
+      );
+    }
   }
 }
--- client/AppliMobile/src/components/BoutonSignalement/stylesBoutonSignalement.js
+++ client/AppliMobile/src/components/BoutonSignalement/stylesBoutonSignalement.js
@@ -1,4 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flex: 1
+  },
+  bouton:{
+    flex: 1
+  }
 });
