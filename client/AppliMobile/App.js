41522bf0b6257a79cfb51b555253822147e46f73
FloMut <34032667+FloMut@users.noreply.github.com>
2019-05-06 14:57:49 +0200

    affichage de toutes les vues façon kendji

--- client/AppliMobile/App.js
+++ client/AppliMobile/App.js
@@ -1,15 +1,68 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { View, Button, AppRegistry } from 'react-native';
 import Accueil from './src/views/Accueil/Accueil';
 import FinTrajet from './src/views/FinTrajet/FinTrajet';
 import Itineraire from './src/views/Itineraire/Itineraire';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Itineraire />
-    );
-  }
+
+    state = {
+        vue : 0
+    }
+
+    _onPress = () => {
+        var newVue = this.state.vue+1;
+        if(newVue == 3)
+            newVue=0;
+        this.setState({vue : newVue});
+    }
+
+    render() {
+        if( this.state.vue == 0 )
+            return (
+                <View style={ {flex:1} } >
+                    <View style={ {flex:1, paddingTop:30} }>
+                        <Button
+                        onPress={this._onPress}
+                        title="Page suivante"
+                        />
+                    </View>
+                    <View style={ {flex:9} } >
+                        <Accueil />
+                    </View>
+                </View>
+            );
+        else if( this.state.vue == 1 )
+            return (
+                <View style={ {flex:1} } >
+                    <View style={ {flex:1, paddingTop:30} }>
+                        <Button
+                        style={ {height:100} }
+                        onPress={this._onPress}
+                        title="Page suivante"
+                        />
+                    </View>
+                    <View style={ {flex:9} } >
+                        <Itineraire />
+                    </View>
+                </View>
+            );
+        else
+            return (
+                <View style={ {flex:1} } >
+                    <View style={ {flex:1, paddingTop:30} }>
+                        <Button
+                        style={ {height:100} }
+                        onPress={this._onPress}
+                        title="Page suivante"
+                        />
+                    </View>
+                    <View style={ {flex:9} } >
+                        <FinTrajet />
+                    </View>
+                </View>
+            );
+    }
 }
 
 AppRegistry.registerComponent('RnDirectionsApp', () => RnDirectionsApp);
